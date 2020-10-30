578ab6bf34dcf10c814fd10c66e5a4dbfb6c9237
Alriightyman <alriightyman@gmail.com>
2020-10-30 13:04:15 -0400

    Added Premake 5 file

---
+++ premake5.lua
@@ -0,0 +1,28 @@
+project "ImGui"
+        kind "StaticLib"
+        language "C++"
+
+        targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+        objdir ("obj/" .. outputDir .. "/%{prj.name}")
+
+        files
+        {
+                "imconfig.h",
+                "imgui.h",
+                "imgui.cpp",
+                "imgui_draw.cpp",
+                "imgui_internal.h",
+                "imgui_widgets.cpp",
+                "imstd_rectpack.h",
+                "imstb_textedit.h",
+                "imstd_truetype.h",
+                "imgui_demo.cpp"
+        }
+
+        fileter "system:windows"
+                systemversion "latest"
+                cppdialect "C++17"
+                staticruntime "On"
+
+        filter {"system:windows", "configurations:Release"}
+                buildoptions "/MT"                
